2bd70a4360976fe4b3184500e8e2c912eaeab497
dyngs <d.skotarczak@ufl.edu>
2023-02-06 11:03:20 +0100

    refactor folder names in tests/

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,10 +6,12 @@ from database import Database
 # setting path
 sys.path.append('../smart_search_beta')
 
+
 def test_create():
     test_database = Database()
     test_database.launch_new_document_store(set_file_name="test1")
     assert test_database.document_store is not None
+
 
 def test_launch_and_update():
     test_database = Database()
@@ -17,8 +19,9 @@ def test_launch_and_update():
     retriever_test = EmbeddingRetriever(document_store=test_database.document_store,
                                         embedding_model="/Users/dyngs/Desktop/IntelligentSearch/smartsearchv0.4/Models/4_eca_retriever_sr_disitlbert-dot-tas_b-b256-msmarco",
                                         model_format="sentence_transformers")
-    test_database.update_document_store('test_data_database', retriever_test)
+    test_database.update_document_store('data_test_database', retriever_test)
     assert test_database.current_number_of_documents > 0
+
 
 def test_save_and_load():
     test_database = Database()
